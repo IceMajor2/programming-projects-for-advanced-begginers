ccfb6049f03e9978792caeeea0a2e8044ef4c178
IceMajor2 <icemajorr2@gmail.com>
2023-03-20 19:43:35 +0100

    Trying to implement nextBoardState() function. Already can list all neighbors for a given cell

--- GameOfLife/src/main/java/GameOfLife.java
+++ GameOfLife/src/main/java/GameOfLife.java
@@ -4,22 +4,22 @@ import java.util.Random;
 public class GameOfLife {
 
     public static void main(String[] args) {
-        render(randomState(7, 4));
+        
     }
 
     public static int[][] randomState(int height, int width) {
         int[][] board = new int[height][width];
         Random random = new Random();
-        for(int i = 0; i < board.length; i++) {
-            for(int y = 0; y < board[i].length; y++) {
-                board[i][y] = random.nextInt(2);
+        for (int i = 0; i < board.length; i++) {
+            for (int y = 0; y < board[i].length; y++) {
+                board[i][y] = random.nextInt(2) == 0 ? -1 : 1;
             }
         }
         return board;
     }
 
     public static void render(int[][] board) {
-        for(int i = 0; i < board[0].length * 2 + 3; i++) {
+        for (int i = 0; i < board[0].length * 2 + 3; i++) {
             System.out.print("-");
         }
         System.out.println("");
@@ -34,9 +34,48 @@ public class GameOfLife {
             }
             System.out.println("|");
         }
-        
-        for(int i = 0; i < board[0].length * 2 + 3; i++) {
+
+        for (int i = 0; i < board[0].length * 2 + 3; i++) {
             System.out.print("-");
         }
+        System.out.println("");
+    }
+
+    public static int[][] nextBoardState(int[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int y = 0; y < board[i].length; y++) {
+                int cell = board[i][y];
+                int[] neighbors = neighbors(board, i, y);
+            }
+        }
+        return board;
+    }
+
+    public static int[] neighbors(int[][] board, int x, int y) {
+        int cell = board[x][y];
+        int[] neighbors = new int[8];
+        for (int pointer = 0, a = x - 1; a < x + 2; a++) {
+            for (int b = y - 1; b < y + 2; b++, pointer++) {
+                if(a == x && b == y) {
+                    continue;
+                }
+                try {
+                    int neighbor = board[a][b];
+                    neighbors[nextFreeIndex(neighbors)] = neighbor;
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    continue;
+                }
+            }
+        }
+        return neighbors;
+    }
+    
+    private static int nextFreeIndex(int[] arr) {
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] == 0) {
+                return i;
+            }
+        }
+        return -1;
     }
 }

c8c56e21325aadd545c034b45044aa308cd98915
IceMajor2 <icemajorr2@gmail.com>
2023-03-20 19:03:51 +0100

    Can randomly create board.

--- GameOfLife/src/main/java/GameOfLife.java
+++ GameOfLife/src/main/java/GameOfLife.java
@@ -1,12 +1,21 @@
 
+import java.util.Random;
 
-/**
- *
- * @author IceMajor
- */
 public class GameOfLife {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int[][] board = randomState(6, 3);
+    }
+    
+    public static int[][] randomState(int height, int width) {
+        int[][] board = new int[height][width];
+        Random random = new Random();
+        for(int[] row : board) {
+            for(int cell : row) {
+                cell = random.nextInt(2);
+                System.out.println(cell);
+            }
+        }
+        return board;
     }
 }

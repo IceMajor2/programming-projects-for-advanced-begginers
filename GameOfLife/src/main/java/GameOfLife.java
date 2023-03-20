ab2b66233ad34bf2886517659c5e5d7f1269884e
IceMajor2 <icemajorr2@gmail.com>
2023-03-20 19:16:09 +0100

    Ability to render.

--- GameOfLife/src/main/java/GameOfLife.java
+++ GameOfLife/src/main/java/GameOfLife.java
@@ -4,18 +4,39 @@ import java.util.Random;
 public class GameOfLife {
 
     public static void main(String[] args) {
-        int[][] board = randomState(6, 3);
+        render(randomState(7, 4));
     }
-    
+
     public static int[][] randomState(int height, int width) {
         int[][] board = new int[height][width];
         Random random = new Random();
-        for(int[] row : board) {
-            for(int cell : row) {
-                cell = random.nextInt(2);
-                System.out.println(cell);
+        for(int i = 0; i < board.length; i++) {
+            for(int y = 0; y < board[i].length; y++) {
+                board[i][y] = random.nextInt(2);
             }
         }
         return board;
+    }
+
+    public static void render(int[][] board) {
+        for(int i = 0; i < board[0].length * 2 + 3; i++) {
+            System.out.print("-");
+        }
+        System.out.println("");
+        for (int[] row : board) {
+            System.out.print("| ");
+            for (int cell : row) {
+                if (cell == 1) {
+                    System.out.print("# ");
+                    continue;
+                }
+                System.out.print("  ");
+            }
+            System.out.println("|");
+        }
+        
+        for(int i = 0; i < board[0].length * 2 + 3; i++) {
+            System.out.print("-");
+        }
     }
 }

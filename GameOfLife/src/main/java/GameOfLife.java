5376e762a030a8d06af78d6d55c4eeff46386c3e
IceMajor2 <icemajorr2@gmail.com>
2023-03-20 20:06:06 +0100

    Changed neighbors() to aliveNeighbors() because the array is actually not needed - count of live cells, however, is.

--- GameOfLife/src/main/java/GameOfLife.java
+++ GameOfLife/src/main/java/GameOfLife.java
@@ -45,15 +45,27 @@ public class GameOfLife {
         for (int i = 0; i < board.length; i++) {
             for (int y = 0; y < board[i].length; y++) {
                 int cell = board[i][y];
-                int[] neighbors = neighbors(board, i, y);
+                int aliveNeighbors = aliveNeighbors(board, i, y);
+                if(cell == 1) {
+                    if(aliveNeighbors <= 1 || aliveNeighbors > 3) {
+                        cell = 0;
+                        board[i][y] = cell;
+                    }
+                    continue;
+                }
+                if(aliveNeighbors == 3) {
+                    cell = 1;
+                    board[i][y] = cell;
+                }
             }
         }
         return board;
     }
 
-    public static int[] neighbors(int[][] board, int x, int y) {
+    public static int aliveNeighbors(int[][] board, int x, int y) {
         int cell = board[x][y];
-        int[] neighbors = new int[8];
+        //int[] neighbors = new int[8];
+        int aliveNeighbors = 0;
         for (int pointer = 0, a = x - 1; a < x + 2; a++) {
             for (int b = y - 1; b < y + 2; b++, pointer++) {
                 if(a == x && b == y) {
@@ -61,21 +73,23 @@ public class GameOfLife {
                 }
                 try {
                     int neighbor = board[a][b];
-                    neighbors[nextFreeIndex(neighbors)] = neighbor;
+                    if(neighbor == 1) {
+                        aliveNeighbors++;
+                    }
                 } catch (ArrayIndexOutOfBoundsException e) {
                     continue;
                 }
             }
         }
-        return neighbors;
+        return aliveNeighbors;
     }
     
-    private static int nextFreeIndex(int[] arr) {
+    /*private static int nextFreeIndex(int[] arr) {
         for(int i = 0; i < arr.length; i++) {
             if(arr[i] == 0) {
                 return i;
             }
         }
         return -1;
-    }
+    }*/
 }

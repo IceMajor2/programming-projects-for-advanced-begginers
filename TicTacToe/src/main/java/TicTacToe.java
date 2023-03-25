fabe9f84f9a61870fbb17a6c64cf7e4899b94e5a
IceMajor2 <icemajorr2@gmail.com>
2023-03-25 13:10:44 +0100

    Game checks the winner (or draw) - fully able to play.

--- TicTacToe/src/main/java/TicTacToe.java
+++ TicTacToe/src/main/java/TicTacToe.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 
 public class TicTacToe {
@@ -5,27 +6,35 @@ public class TicTacToe {
     public static void main(String[] args) {
         char[][] board = new char[3][3];
         render(board);
-        
+
         char player = 'X';
-        while(true) {
+        int moves = 0;
+        while (moves != 9 && moves < 5 || winner(board) == '\0') { // double OR condition is there so the winner is not checked unneccessarily when there fewer than 5 moves
             makeMove(board, player);
+            moves++;
             render(board);
             player = (player == 'X') ? 'O' : 'X';
         }
+        char winner = winner(board);
+        if (winner == '\0') {
+            System.out.println("DRAW");
+        } else {
+            System.out.println(winner + " WINS");
+        }
     }
-    
+
     public static void makeMove(char[][] board, char player) {
         Scanner scanner = new Scanner(System.in);
-        while(true) {
+        while (true) {
             System.out.print("Your move (ex: 1 2): ");
             int x = scanner.nextInt();
             int y = scanner.nextInt();
             scanner.nextLine();
-            if(x < 0 || x > 2 || y < 0 || y > 2) {
+            if (x < 0 || x > 2 || y < 0 || y > 2) {
                 System.out.println("Stick within the board.");
                 continue;
             }
-            if(board[x][y] != '\0') {
+            if (board[x][y] != '\0') {
                 System.out.println("Place is occupied.");
                 continue;
             }
@@ -33,13 +42,13 @@ public class TicTacToe {
             break;
         }
     }
-    
+
     public static void render(char[][] board) {
         System.out.println("---------");
-        for(char[] row : board) {
+        for (char[] row : board) {
             System.out.print("| ");
-            for(char place : row) {
-                if(place == '\0') {
+            for (char place : row) {
+                if (place == '\0') {
                     System.out.print("  ");
                     continue;
                 }
@@ -48,5 +57,51 @@ public class TicTacToe {
             System.out.println("|");
         }
         System.out.println("---------");
+    }
+
+    public static char winner(char[][] board) {
+        char horizontalWinner = horizontalWinner(board);
+        if (horizontalWinner != '\0') {
+            return horizontalWinner;
+        }
+        char verticalWinner = verticalWinner(board);
+        if (verticalWinner != '\0') {
+            return verticalWinner;
+        }
+        char diagonalWinner = diagonalWinner(board);
+        if (diagonalWinner != '\0') {
+            return diagonalWinner;
+        }
+        return '\0';
+    }
+
+    public static char horizontalWinner(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][0] == board[i][1] && board[i][0] == board[i][2]) {
+                return board[i][0];
+            }
+        }
+        return '\0';
+    }
+
+    public static char verticalWinner(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int y = 0; y < board[i].length; y++) {
+                if (board[0][y] == board[1][y] && board[0][y] == board[2][y]) {
+                    return board[0][y];
+                }
+            }
+        }
+        return '\0';
+    }
+
+    public static char diagonalWinner(char[][] board) {
+        if (board[0][0] == board[1][1] && board[0][0] == board[2][2]) {
+            return board[0][0];
+        }
+        if (board[0][2] == board[1][1] && board[0][2] == board[2][0]) {
+            return board[0][2];
+        }
+        return '\0';
     }
 }

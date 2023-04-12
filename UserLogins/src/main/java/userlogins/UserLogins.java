3a03bb995f9319d4cf32d4700074a25a520d257b
IceMajor <icemajorr2@gmail.com>
2023-04-12 16:50:05 +0200

    Passwords stored as SHA256 hashes.

--- UserLogins/src/main/java/userlogins/UserLogins.java
+++ UserLogins/src/main/java/userlogins/UserLogins.java
@@ -3,10 +3,15 @@ package userlogins;
 import java.util.Scanner;
 import java.util.Map;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.nio.charset.StandardCharsets;
+import java.security.NoSuchAlgorithmException;
+
 public class UserLogins {
 
     public static Map<String, String> credentialsDb
-            = Map.of("ping", "pong", "ching", "chang");
+            = Map.of("ping", getSHA256("pong"), "ching", getSHA256("chang"));
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -25,6 +30,26 @@ public class UserLogins {
 
     public static boolean credentialsValid(String usr, String pass) {
         return credentialsDb.containsKey(usr)
-                && credentialsDb.containsValue(pass);
+                && credentialsDb.containsValue(getSHA256(pass));
+    }
+
+    public static byte[] bytesSHA256(String password) {
+        try {
+            MessageDigest md5 = MessageDigest.getInstance("SHA-256");
+            return md5.digest(password.getBytes(StandardCharsets.UTF_8));
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    public static String getSHA256(String password) {
+        byte[] bytesSHA = bytesSHA256(password);
+        BigInteger number = new BigInteger(1, bytesSHA);
+        StringBuilder hexString = new StringBuilder(number.toString(16));
+        
+        while(hexString.length() < 64) {
+            hexString.insert(0, '0');
+        }
+        return hexString.toString();
     }
 }

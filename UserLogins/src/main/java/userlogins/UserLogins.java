c5f2c30271379a38715ac268fb49d4f8e7e4eb4e
IceMajor <icemajorr2@gmail.com>
2023-04-12 15:46:08 +0200

    Something... log in?

--- UserLogins/src/main/java/userlogins/UserLogins.java
+++ UserLogins/src/main/java/userlogins/UserLogins.java
@@ -1,9 +1,25 @@
-
 package userlogins;
+
+import java.util.Scanner;
 
 public class UserLogins {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter username: ");
+        String username = scanner.nextLine();
+        System.out.print("Enter password: ");
+        String password = scanner.nextLine();
         
+        if (credentialsValid(username, password)) {
+            System.out.println("Secret world accessed");
+        } else {
+            System.out.println("Fuck off you");
+        }
+    }
+
+    public static boolean credentialsValid(String usr, String pass) {
+        return usr.equals("ping") && pass.equals("pong");
     }
 }

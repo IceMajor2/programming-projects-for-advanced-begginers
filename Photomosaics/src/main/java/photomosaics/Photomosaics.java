b443e11c5bf948555204ba8b92312c6fb0ae3c1c
IceMajor <icemajorr2@gmail.com>
2023-04-16 12:44:01 +0200

    NB project create

---
+++ Photomosaics/src/main/java/photomosaics/Photomosaics.java
@@ -0,0 +1,14 @@
+
+
+package photomosaics;
+
+/**
+ *
+ * @author IceMajor
+ */
+public class Photomosaics {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

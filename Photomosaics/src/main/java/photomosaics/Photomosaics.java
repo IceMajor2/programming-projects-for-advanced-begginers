973d5d78a56edb18719ba2084a06234faea747fe
IceMajor <icemajorr2@gmail.com>
2023-04-16 13:08:59 +0200

    Getting to know imgscalr lib (no more OpenPNP). Output of resized img.

--- Photomosaics/src/main/java/photomosaics/Photomosaics.java
+++ Photomosaics/src/main/java/photomosaics/Photomosaics.java
@@ -1,9 +1,22 @@
-
 package photomosaics;
+
+import org.imgscalr.Scalr;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class Photomosaics {
 
-    public static void main(String[] args) {
-        
+    public static File inputPic = new File("pictures" + File.separator
+            + "input" + File.separator + "nature.jpg");
+    public static File outputPic = new File("pictures" + File.separator
+            + "output" + File.separator + "output.jpg");
+
+    public static void main(String[] args) throws IOException {
+        BufferedImage pic = ImageIO.read(inputPic);
+        BufferedImage scaledPic = Scalr.resize(pic, 150);
+        ImageIO.write(scaledPic, "jpg", outputPic);
+
     }
 }

77b215dee300a4316c3c9ec53ac8c82946df9181
IceMajor <icemajorr2@gmail.com>
2023-04-16 12:52:29 +0200

    Provide exemplary pic, get image processing library OpenPNP

--- Photomosaics/src/main/java/photomosaics/Photomosaics.java
+++ Photomosaics/src/main/java/photomosaics/Photomosaics.java
@@ -1,14 +1,9 @@
 
-
 package photomosaics;
 
-/**
- *
- * @author IceMajor
- */
 public class Photomosaics {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
     }
 }

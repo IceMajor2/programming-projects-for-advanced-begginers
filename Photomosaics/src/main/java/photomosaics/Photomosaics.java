5197f660a9da4c7bf795769e17c5b8e4d33dd286
IceMajor <icemajorr2@gmail.com>
2023-04-17 17:03:52 +0200

    The divisibility limit is no longer present. Groups of pixels adjust to the bounds of images

--- Photomosaics/src/main/java/photomosaics/Photomosaics.java
+++ Photomosaics/src/main/java/photomosaics/Photomosaics.java
@@ -2,6 +2,8 @@ package photomosaics;
 
 import org.imgscalr.Scalr;
 import java.awt.image.BufferedImage;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.IOException;
@@ -11,10 +13,6 @@ import java.awt.Graphics2D;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 
-/*
-TODO:
-    - fix the not-quite-working imgs not divisible in <45; 55> range
- */
 public class Photomosaics {
 
     public static String PATH_TO_INPUT = String.format("%s%s%s%s", "pictures",
@@ -25,16 +23,15 @@ public class Photomosaics {
             File.separator, "dataset", File.separator);
 
     public static void main(String[] args) throws IOException {
-        BufferedImage picSource = ImageIO.read(new File(PATH_TO_INPUT + "nature.jpg"));
-        int groupDim = determinePixelGroupSideLength(picSource);
-        System.out.println(groupDim);
-        //int groupDim = 25;
-        double[][][] avgs = getPixelGroupsColor(picSource, groupDim);
+        BufferedImage picSource = ImageIO.read(new File(PATH_TO_INPUT + "sf.jpg"));
+        //int groupDim = calculatePxGroupDimension(picSource);
+        int groupDim = 5;
+        int[][][] avgs = pixelGroupsColorAvg(picSource, groupDim);
         BufferedImage pixelated = pixelate(picSource, avgs, groupDim);
         ImageIO.write(pixelated, "jpg", new File(PATH_TO_OUTPUT + "pixelated.jpg"));
     }
 
-    private static int determinePixelGroupSideLength(BufferedImage img) {
+    public static int calculatePxGroupDimension(BufferedImage img) {
         int picHeight = img.getHeight();
         int picWidth = img.getWidth();
 
@@ -53,49 +50,82 @@ public class Photomosaics {
         return equivalentLength;
     }
 
-    public static double[][][] getPixelGroupsColor(BufferedImage img, int groupDim) {
-        int rows = img.getHeight() / groupDim;
-        int columns = img.getWidth() / groupDim;
-        double[][][] groups = new double[rows][columns][3];
+    public static int[][][] pixelGroupsColorAvg(BufferedImage img, int groupDim) {
+        int picWidth = img.getWidth();
+        int picHeight = img.getHeight();
 
-        int row = 0;
-        while (row < rows) {
+        double rows = img.getHeight() / (double) groupDim;
+        double columns = img.getWidth() / (double) groupDim;
+        BigDecimal rowsBd = BigDecimal.valueOf(rows);
+        BigDecimal columnsBd = BigDecimal.valueOf(columns);
+        rowsBd = rowsBd.setScale(0, RoundingMode.CEILING);
+        columnsBd = columnsBd.setScale(0, RoundingMode.CEILING);
+        rows = rowsBd.doubleValue();
+        columns = columnsBd.doubleValue();
+        int[][][] allGroups = new int[(int) rows][(int) columns][3];
+
+        for (int row = 0; row < rows; row++) {
+
             for (int column = 0; column < columns; column++) {
 
-                double avgRed = 0;
-                double avgGreen = 0;
-                double avgBlue = 0;
+                int gColumns = groupDim;
+                int gRows = groupDim;
+                if (column == columns - 1) {
+                    gColumns = picWidth - (column * groupDim);
+                }
+                if (row == rows - 1) {
+                    gRows = picHeight - (row * groupDim);
+                }
+                int[][] group = new int[gRows][gColumns];
+                for (int x = column * groupDim; x < column * groupDim + gColumns
+                        && x < picWidth; x++) {
 
-                for (int x = column * groupDim; x < column * groupDim + groupDim; x++) {
-
-                    for (int y = row * groupDim; y < row * groupDim + groupDim; y++) {
-
-                        int pixel = img.getRGB(x, y);
-                        Color c = new Color(pixel);
-                        int red = c.getRed();
-                        int green = c.getGreen();
-                        int blue = c.getBlue();
-                        avgRed += red;
-                        avgGreen += green;
-                        avgBlue += blue;
-
+                    for (int y = row * groupDim; y < row * groupDim + gRows
+                            && y < picHeight; y++) {
+                        group[y % groupDim][x % groupDim] = img.getRGB(x, y);
                     }
                 }
+                int[] avgRGB = colorAvgOfPixelGroup(group);
+                int red = avgRGB[0];
+                int green = avgRGB[1];
+                int blue = avgRGB[2];
 
-                avgRed = avgRed / (double) (groupDim * groupDim);
-                avgGreen = avgGreen / (double) (groupDim * groupDim);
-                avgBlue = avgBlue / (double) (groupDim * groupDim);
-                groups[row][column][0] = avgRed;
-                groups[row][column][1] = avgGreen;
-                groups[row][column][2] = avgBlue;
+                allGroups[row][column][0] = red;
+                allGroups[row][column][1] = green;
+                allGroups[row][column][2] = blue;
             }
-            row++;
         }
-        return groups;
+        return allGroups;
     }
 
-    public static BufferedImage pixelate(BufferedImage img, double[][][] pxGroup, int groupDim) {
-        BufferedImage copy = copyImage(img);
+    public static int[] colorAvgOfPixelGroup(int[][] pixelGroup) {
+        double redTotal = 0;
+        double greenTotal = 0;
+        double blueTotal = 0;
+
+        int iterations = 0;
+        for (int i = 0; i < pixelGroup.length; i++) {
+            for (int y = 0; y < pixelGroup[i].length; y++) {
+                Color c = new Color(pixelGroup[i][y]);
+                int red = c.getRed();
+                int green = c.getGreen();
+                int blue = c.getBlue();
+                redTotal += red;
+                greenTotal += green;
+                blueTotal += blue;
+                iterations++;
+            }
+        }
+
+        int redAvg = (int) redTotal / iterations;
+        int greenAvg = (int) greenTotal / iterations;
+        int blueAvg = (int) blueTotal / iterations;
+
+        return new int[]{redAvg, greenAvg, blueAvg};
+    }
+
+    public static BufferedImage pixelate(BufferedImage img, int[][][] pxGroup, int groupDim) {
+        BufferedImage copy = copy(img);
         Graphics2D graph = copy.createGraphics();
         for (int row = 0; row < pxGroup.length; row++) {
 
@@ -112,7 +142,7 @@ public class Photomosaics {
         return copy;
     }
 
-    public static BufferedImage copyImage(BufferedImage source) {
+    public static BufferedImage copy(BufferedImage source) {
         BufferedImage b = new BufferedImage(source.getWidth(), source.getHeight(), source.getType());
         Graphics g = b.getGraphics();
         g.drawImage(source, 0, 0, null);

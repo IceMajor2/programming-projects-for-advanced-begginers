d720e821eecc99988c6b809b46028d2d94eb10fe
IceMajor <icemajorr2@gmail.com>
2023-04-18 11:30:04 +0200

    User can determine "photo pixel" size

--- Photomosaics/src/main/java/photomosaics/UserInterface.java
+++ Photomosaics/src/main/java/photomosaics/UserInterface.java
@@ -3,6 +3,7 @@ package photomosaics;
 import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
+import java.awt.image.BufferedImage;
 
 public class UserInterface {
 
@@ -48,15 +49,31 @@ public class UserInterface {
                 System.out.println("Couldn't find image. (Remember about file extension)");
                 continue;
             }
+
             String format = imageFile.getName().substring(
                     imageFile.getName().lastIndexOf('.') + 1);
             String nameWithoutFormat = imageFile.getName().substring(0,
                     imageFile.getName().lastIndexOf('.'));
             var img = DataHandler.readImg(imageFile);
+
+            System.out.println("Give size of a \"photo pixel\" size. (Leave empty for automatic estimate)");
+            System.out.print("> ");
+            String input = scanner.nextLine();
+
+            BufferedImage mosaic = null;
+            System.out.println("Creating photomosaic...");
+            if (!input.isEmpty()) {
+                int groupDim = Integer.valueOf(scanner.nextLine());
+                mosaic = ImageHandler.photomosaic(img, groupDim);
+            } else {
+                mosaic = ImageHandler.photomosaic(img);
+            }
+
             try {
-                var mosaic = ImageHandler.photomosaic(img);
                 DataHandler.outputImg(mosaic, String.format("%s_pm.%s",
                         nameWithoutFormat, format), format);
+                System.out.println("Success!"
+                        + " See the output in the \"output\" folder.");
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }

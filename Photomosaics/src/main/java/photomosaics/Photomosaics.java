ef949d356943a355e7015d3feb953fef732505fe
IceMajor <icemajorr2@gmail.com>
2023-04-18 16:00:09 +0200

    New TODO

--- Photomosaics/src/main/java/photomosaics/Photomosaics.java
+++ Photomosaics/src/main/java/photomosaics/Photomosaics.java
@@ -2,6 +2,7 @@ package photomosaics;
 /*
 TODO:
     - The database should be loading and/or deleting only those files that need to be. There's no point in writing the whole yml file.
+    - IF I want to speed up making a photomosaic process, I could - when the user enters pixel group size - recalculate the avg RGB color for each image in a given size.
  */
 public class Photomosaics {
 

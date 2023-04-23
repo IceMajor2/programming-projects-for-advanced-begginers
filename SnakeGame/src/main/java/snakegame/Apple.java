11ee59a6ada3992fc33fe66dc6cc821edc3aa529
IceMajor <icemajorr2@gmail.com>
2023-04-23 14:34:52 +0200

    Apple class stores position

--- SnakeGame/src/main/java/snakegame/Apple.java
+++ SnakeGame/src/main/java/snakegame/Apple.java
@@ -1,5 +1,14 @@
 package snakegame;
 
 public class Apple {
-    
+
+    private int[] position;
+
+    public Apple(int x, int y) {
+        position = new int[]{x, y};
+    }
+
+    public int[] getPosition() {
+        return position;
+    }
 }
